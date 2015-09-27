15e89dd41a21d6bf0aaa73c30f1083a9fa443a83
Giselle Tavares <gitavares@hotmail.com>
2015-09-27 17:14:48 -0300

    incluido boostrap

---
+++ Controllers/CategoryEnum.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel;
+using System.Reflection;
+
+public enum Category
+{
+	[DescriptionAttribute("Habitação")]HABITACAO = 1, 
+	
+	[DescriptionAttribute("Transporte")]TRANSPORTE = 2,
+	
+	[DescriptionAttribute("Saúde")]SAUDE = 3,
+	
+	[DescriptionAttribute("Educação")]EDUCACAO = 4,
+	
+	[DescriptionAttribute("Impostos")]IMPOSTOS = 5,
+	
+	[DescriptionAttribute("Alimentação")]ALIMENTACAO = 6,
+	
+	[DescriptionAttribute("Cuidados Pessoais")]CUIDADOSPESSOAIS = 7,
+	
+	[DescriptionAttribute("Manutenção")]MANUTENCAO = 8, 
+	
+	[DescriptionAttribute("Lazer")]LAZER = 9,
+	
+	[DescriptionAttribute("Vestuário")]VESTUARIO = 10,
+	
+	[DescriptionAttribute("Outros")]OUTROS = 999
+}
