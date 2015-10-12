4bb66e5c22f59bce9c8283a1b45abd320a8343b3
Giselle Tavares <gitavares@hotmail.com>
2015-10-12 16:40:02 -0300

    Testando a aplicacao

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -10,25 +10,5 @@ namespace tiqe_web.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public int Id {get;}
-        public string Picture{get; set;}
-        [RequiredAttribute]
-        public string FirstName{get; set;}
-        [RequiredAttribute]
-        public string LastName{get; set;}
-        [RequiredAttribute]
-        public string Email{get; set;}
-        [RequiredAttribute]
-        public string Pass {get; set;}
-        public DateTime Birthday{get; set;}
-        [RequiredAttribute]
-        public bool UseTerms{get; set;}
-        public bool Status {get; set;}
-        public bool FirstLogin {get; set;}
-        public string Language {get;}
-        public bool SendLogError{get; set;}
-        public DateTime RegisterDate {get;}
-        public DateTime ModifyDate {get; set;}
-        
     }
 }
