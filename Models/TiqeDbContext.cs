67e307d61d28af0f59f11670c143c520afd4dc50
gitavares <gitavares@hotmail.com>
2016-04-25 15:21:33 -0300

    Created the application DBContext

---
+++ Models/TiqeDbContext.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Linq;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Configuration;
+
+namespace tiqe_web.Models
+{
+    public class TiqeDbContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+        //colocar as demais tabelas aqui
+		
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>().HasKey(m => m.Id);
+            //colocar as demais tabelas aqui
+            
+            builder.Entity<User>().Property<DateTime>("UpdatedTimestamp");
+            //colocar as demais tabelas aqui
+            
+            base.OnModelCreating(builder);
+        }
+        
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var builder = new ConfigurationBuilder()
+            .AddJsonFile("../config.json")
+            .AddEnvironmentVariables();
+            
+            var configuration = builder.Build();
+            
+            var sqlConnectionString = configuration["DefaultConnection:ConnectionString"];
+            
+            optionsBuilder.UseNpgsql(sqlConnectionString);
+		}
+        
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+            
+            updateUpdatedProperty<User>();
+            //colocar as demais tabelas aqui
+            
+            return base.SaveChanges();
+        }
+        
+        private void updateUpdatedProperty<T>() where T : class
+        {
+            //ver direito como isso funciona. 
+            var modifiedUser = ChangeTracker.Entries<T>()
+            .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+            
+            foreach (var entry in modifiedUser)
+            {
+                //aqui ele atualizaria todas as datas?? não quero assim. ver com funciona
+                entry.Property("UpdatedTimestamp").CurrentValue = DateTime.UtcNow;
+            }
+        }
+	}
+}
